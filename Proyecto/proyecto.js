efb628bf1454bcf77cc10daee8e32c76278f109f
Matias <gonzalezmatias6164@gmail.com>
2024-07-26 05:45:36 -0300

    Unificando footer

--- Proyecto/proyecto.js
+++ Proyecto/proyecto.js
@@ -1,10 +1,13 @@
-function toggleSkills(tipo) {
+function mostrarHabilidades(tipo) {
     if (tipo === 'tecnica') {
         var habilidadesTecnicas = document.getElementById('habilidades-tecnicas');
         habilidadesTecnicas.style.display = (habilidadesTecnicas.style.display === 'none') ? 'block' : 'none';
     } else if (tipo === 'blanda') {
         var habilidadesBlandas = document.getElementById('habilidades-blandas');
         habilidadesBlandas.style.display = (habilidadesBlandas.style.display === 'none') ? 'block' : 'none';
+    }else if (tipo==='otra'){
+        var otrasHabilidades=document.getElementById('otras');
+        otrasHabilidades.style.display=(otrasHabilidades.style.display==='none')? 'block' : 'none';
     }
 }
 
